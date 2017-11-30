59687da70c3309745658ddc8ac10e4293890ef4a
wheatless <ireucroft@gmail.com>
2017-11-30 07:52:08 -0500

    Completed basic history dependence

--- glm.m
+++ glm.m
@@ -1,4 +1,4 @@
-function [b1, dev1, stats1, lambda1, b2, dev2, stats2, lambda2, b3, dev3, stats3, lambda3, b4, dev4, stats4, lambda4, b5, dev5, stats5, lambda5] = glm(file)
+function [b, dev, stats, lambda] = glm(file)
 % Fits GLM for 3 specified models to data in file
 
 % Error condition
@@ -8,6 +8,11 @@ if ~isa(file,'char')
 end
 
 load(file);
+
+b = {};
+dev = {};
+stats = {};
+lambda = {};
 
 %---------------- Model 1, Covariates: X, Y, X^2, Y^2 --------------------
 
@@ -41,7 +46,10 @@ for i = 1:length(spikes_binned(1,:))
     disp(['Completed Neuron ' num2str(i) '.']);
 end
 
-
+b{1} = b1;
+dev{1} = dev1;
+stats{1} = stats1;
+lambda{1} = lambda1;
 
 
 %---------------- Model 2, Covariates: Sinusoids ---------------
@@ -75,115 +83,226 @@ for i = 1:length(spikes_binned(1,:))
     disp(['Completed Neuron ' num2str(i) '.']);
 end
 
-
+b{2} = b2;
+dev{2} = dev2;
+stats{2} = stats2;
+lambda{2} = lambda2;
 
 %---------------- Model 3, Covariates: Refractory period history-dependence ---------------
-
-% Positions to plot
-figure;
-[x_new,y_new]=meshgrid(-1:.1:1);
-y_new = flipud(y_new);
-x_new = fliplr(x_new);
+% Using history from 1 ms to 5 ms
 
 for i = 1:length(spikes_binned(1,:))
+    % Defining history
+    lambh = zeros(length(spikes_binned(:,i))-5,length(spikes_binned(1:5,i)));
+    for k = 1:length(spikes_binned(1:5,i))
+        lambh(:,k) = spikes_binned((6-k):(end-k),i);
+    end
     
     % GLM coefficients
-    [b3(:,i),dev3(:,i),stats3(i)] = glmfit([xN(6:end) yN(6:end) xN(6:end).^2 yN(6:end).^2 ones(size(xN(6:end))).*spikes_binned(1:5,i)'],spikes_binned(6:end,i),'poisson');
-
+    [b3(:,i),dev3(:,i),stats3(i)] = glmfit([lambh],spikes_binned(6:end,i),'poisson');
     % Computing lambda
-    % History-dependent portion
-    lambh = 0;
-    for k = 1:length(spikes_binned(1:5,i))
-    lambh = lambh + b3(k+5,i)*spikes_binned(k,i)';
+    % Lambda without history
+    lamb = exp(b3(1,i));
+    % Adding history components
+    for k = 2:length(b3(:,i))
+     lamb = lamb + b3(k,i)*lambh(:,k-1);
     end
-    lamb = exp(b3(1,i) + b3(2,i)*x_new + b3(3,i)*y_new + b3(4,i).*x_new.^2 + b3(5,i).*y_new.^2 + lambh);
-    lamb(find(x_new.^2+y_new.^2>1))=nan;
+    
     lambda3(:,:,i) = lamb;
-
-    % Plotting lambda and circle defining position limits
-    subplot(2,5,i)
-    h_mesh = mesh(x_new,y_new,lambda3(:,:,i),'AlphaData',0);
-    get(h_mesh,'AlphaData');
-    set(h_mesh,'AlphaData',0);
-    xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
-    hold on;
-    plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
-
     
     disp(['Completed Neuron ' num2str(i) '.']);
 end
+
+b{3} = b3;
+dev{3} = dev3;
+stats{3} = stats3;
+lambda{3} = lambda3;
+
+% POSITION AND HISTORY DEPENDENCE
+% % Positions to plot
+% figure;
+% [x_new,y_new]=meshgrid(-1:.1:1);
+% y_new = flipud(y_new);
+% x_new = fliplr(x_new);
+% 
+% for i = 1:length(spikes_binned(1,:))
+%     % Defining history components
+%     lambh = zeros(length(spikes_binned(:,i))-5,length(spikes_binned(1:5,i)));
+%     for k = 1:length(spikes_binned(1:5,i))
+%         lambh(:,k) = spikes_binned((6-k):(end-k),i);
+%     end
+%     
+%     % GLM coefficients
+%     [b3(:,i),dev3(:,i),stats3(i)] = glmfit([xN(6:end) yN(6:end) xN(6:end).^2 yN(6:end).^2 lambh],spikes_binned(6:end,i),'poisson');
+%     disp(b3);
+%     % Computing lambda
+%     % Lambda without history
+%     lamb = exp(b3(1,i) + b3(2,i)*x_new + b3(3,i)*y_new + b3(4,i).*x_new.^2 + b3(5,i).*y_new.^2);
+%     % Adding history components
+%     for k = 6:length(b3(:,i))
+%      lamb = lamb + b3(k,i)*lambh(:,k-5);
+%     end
+% 
+%     lamb(find(x_new.^2+y_new.^2>1))=nan;
+%     lambda3(:,:,i) = lamb;
+% 
+%     % Plotting lambda and circle defining position limits
+%     subplot(2,5,i)
+%     h_mesh = mesh(x_new,y_new,lambda3(:,:,i),'AlphaData',0);
+%     get(h_mesh,'AlphaData');
+%     set(h_mesh,'AlphaData',0);
+%     xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
+%     hold on;
+%     plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
+% 
+%     
+%     disp(['Completed Neuron ' num2str(i) '.']);
+% end
 
 %---------------- Model 4, Covariates: Short-term history-dependence ---------------
-% Using history from 75 ms to 175 ms
-% Positions to plot
-figure;
-[x_new,y_new]=meshgrid(-1:.1:1);
-y_new = flipud(y_new);
-x_new = fliplr(x_new);
+% Using history from 75 ms to 125 ms
 
 for i = 1:length(spikes_binned(1,:))
+    % Defining history
+    lambh = zeros(length(spikes_binned(:,i))-125,length(spikes_binned(75:125,i)));
+    for k = 1:length(spikes_binned(75:125,i))
+        lambh(:,k) = spikes_binned((126-k):(end-k),i);
+    end
     
     % GLM coefficients
-    [b4(:,i),dev4(:,i),stats4(i)] = glmfit([xN(176:end) yN(176:end) xN(176:end).^2 yN(176:end).^2 ones(size(xN(176:end))).*spikes_binned(75:175,i)'],spikes_binned(176:end,i),'poisson');
-
+    [b4(:,i),dev4(:,i),stats4(i)] = glmfit([lambh],spikes_binned(126:end,i),'poisson');
     % Computing lambda
-    % History-dependent portion
-    lambh = 0;
-    for k = 1:length(spikes_binned(75:175,i))
-    lambh = lambh + b4(k+5,i)*spikes_binned(k,i)';
+    % Lambda without history
+    lamb = exp(b4(1,i));
+    % Adding history components
+    for k = 2:length(b4(:,i))
+     lamb = lamb + b4(k,i)*lambh(:,k-1);
     end
-    lamb = exp(b4(1,i) + b4(2,i)*x_new + b4(3,i)*y_new + b4(4,i).*x_new.^2 + b4(5,i).*y_new.^2 + lambh);
-    lamb(find(x_new.^2+y_new.^2>1))=nan;
+    
     lambda4(:,:,i) = lamb;
-
-    % Plotting lambda and circle defining position limits
-    subplot(2,5,i)
-    h_mesh = mesh(x_new,y_new,lambda4(:,:,i),'AlphaData',0);
-    get(h_mesh,'AlphaData');
-    set(h_mesh,'AlphaData',0);
-    xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
-    hold on;
-    plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
-
     
     disp(['Completed Neuron ' num2str(i) '.']);
 end
+
+b{4} = b4;
+dev{4} = dev4;
+stats{4} = stats4;
+lambda{4} = lambda4;
+
+% POSITION AND HISTORY DEPENDENCE
+% % Using history from 75 ms to 175 ms
+% % Positions to plot
+% figure;
+% [x_new,y_new]=meshgrid(-1:.1:1);
+% y_new = flipud(y_new);
+% x_new = fliplr(x_new);
+% 
+% 
+% for i = 1:length(spikes_binned(1,:))
+%     % Defining history components
+%     lambh = zeros(length(spikes_binned(:,i))-175,length(spikes_binned(75:175,i)));
+%     for k = 1:length(spikes_binned(75:175,i))
+%         lambh(:,k) = spikes_binned((176-k):(end-k),i);
+%     end
+%     
+%     % GLM coefficients
+%     [b4(:,i),dev4(:,i),stats4(i)] = glmfit([xN(176:end) yN(176:end) xN(176:end).^2 yN(176:end).^2 lambh],spikes_binned(176:end,i),'poisson');
+% 
+%     % Computing lambda
+%     % Lambda without history
+%     lamb = exp(b4(1,i) + b4(2,i)*x_new + b4(3,i)*y_new + b4(4,i).*x_new.^2 + b4(5,i).*y_new.^2);
+%     % Adding history components
+%     for k = 6:length(b4(:,i))
+%      lamb = lamb + b4(k,i).*lambh(:,k-5);
+%     end
+%     
+%     lamb(find(x_new.^2+y_new.^2>1))=nan;
+%     lambda4(:,:,i) = lamb;
+% 
+%     % Plotting lambda and circle defining position limits
+%     subplot(2,5,i)
+%     h_mesh = mesh(x_new,y_new,lambda4(:,:,i),'AlphaData',0);
+%     get(h_mesh,'AlphaData');
+%     set(h_mesh,'AlphaData',0);
+%     xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
+%     hold on;
+%     plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
+% 
+%     
+%     disp(['Completed Neuron ' num2str(i) '.']);
+% end
+
 
 %---------------- Model 5, Covariates: Long-term history-dependence ---------------
 % Using history from 200 ms to 275 ms
-% Positions to plot
-figure;
-[x_new,y_new]=meshgrid(-1:.1:1);
-y_new = flipud(y_new);
-x_new = fliplr(x_new);
 
 for i = 1:length(spikes_binned(1,:))
+    % Defining history
+    lambh = zeros(length(spikes_binned(:,i))-275,length(spikes_binned(200:275,i)));
+    for k = 1:length(spikes_binned(200:275,i))
+        lambh(:,k) = spikes_binned((276-k):(end-k),i);
+    end
     
     % GLM coefficients
-    [b5(:,i),dev5(:,i),stats5(i)] = glmfit([xN(201:end) yN(201:end) xN(201:end).^2 yN(201:end).^2 ones(size(xN(201:end))).*spikes_binned(200:275,i)'],spikes_binned(201:end,i),'poisson');
-
+    [b5(:,i),dev5(:,i),stats5(i)] = glmfit([lambh],spikes_binned(276:end,i),'poisson');
     % Computing lambda
-    % History-dependent portion
-    lambh = 0;
-    for k = 1:length(spikes_binned(200:275,i))
-    lambh = lambh + b5(k+5,i)*spikes_binned(k,i)';
+    % Lambda without history
+    lamb = exp(b5(1,i));
+    % Adding history components
+    for k = 2:length(b5(:,i))
+     lamb = lamb + b5(k,i)*lambh(:,k-1);
     end
-    lamb = exp(b5(1,i) + b5(2,i)*x_new + b5(3,i)*y_new + b5(4,i).*x_new.^2 + b5(5,i).*y_new.^2 + lambh);
-    lamb(find(x_new.^2+y_new.^2>1))=nan;
+    
     lambda5(:,:,i) = lamb;
-
-    % Plotting lambda and circle defining position limits
-    subplot(2,5,i)
-    h_mesh = mesh(x_new,y_new,lambda5(:,:,i),'AlphaData',0);
-    get(h_mesh,'AlphaData');
-    set(h_mesh,'AlphaData',0);
-    xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
-    hold on;
-    plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
-
     
     disp(['Completed Neuron ' num2str(i) '.']);
 end
+
+b{5} = b5;
+dev{5} = dev5;
+stats{5} = stats5;
+lambda{5} = lambda5;
+
+% % Using history from 200 ms to 275 ms
+% % Positions to plot
+% figure;
+% [x_new,y_new]=meshgrid(-1:.1:1);
+% y_new = flipud(y_new);
+% x_new = fliplr(x_new);
+% 
+% for i = 1:length(spikes_binned(1,:))
+%     % Defining history components
+%     lambh = zeros(length(spikes_binned(:,i))-275,length(spikes_binned(200:275,i)));
+%     for k = 1:length(spikes_binned(200:275,i))
+%         lambh(:,k) = spikes_binned((276-k):(end-k),i);
+%     end
+%     
+%     % GLM coefficients
+%     [b5(:,i),dev5(:,i),stats5(i)] = glmfit([xN(201:end) yN(201:end) xN(201:end).^2 yN(201:end).^2 lambh],spikes_binned(201:end,i),'poisson');
+% 
+%     % Computing lambda
+%     % Lambda without history
+%     lamb = exp(b5(1,i) + b5(2,i)*x_new + b5(3,i)*y_new + b5(4,i).*x_new.^2 + b5(5,i).*y_new.^2);
+%     % Adding history components
+%     for k = 6:length(b5(:,i))
+%      lamb = lamb + b5(k,i).*lambh(:,k-5);
+%     end
+% 
+%     lamb(find(x_new.^2+y_new.^2>1))=nan;
+%     lambda5(:,:,i) = lamb;
+% 
+%     % Plotting lambda and circle defining position limits
+%     subplot(2,5,i)
+%     h_mesh = mesh(x_new,y_new,lambda5(:,:,i),'AlphaData',0);
+%     get(h_mesh,'AlphaData');
+%     set(h_mesh,'AlphaData',0);
+%     xlabel('x'); ylabel('y'); title(['Lambda, Neuron ' num2str(i)]);
+%     hold on;
+%     plot3(cos(-pi:1e-2:pi),sin(-pi:1e-2:pi),zeros(size(-pi:1e-2:pi)));
+% 
+%     
+%     disp(['Completed Neuron ' num2str(i) '.']);
+% end
 
 
 
---
+++ ks_aic.m
@@ -0,0 +1,369 @@
+function [ks_stat, KSSorted, AIC] = ks_aic(file,b,dev,stats)
+% Creates KS plot for data in file
+
+% Error condition
+if ~isa(file,'char')
+    disp('Input argument must be in form ''[filename].mat''');
+    return
+end
+
+load(file);
+
+ks_stat = {};
+KSSorted = {};
+AIC = {};
+
+%---------------- Model 1, Covariates: X, Y, X^2, Y^2 --------------------
+
+% Confidence intervals for each covariate
+figure(1);
+for i = 1:length(b{1}(1,:))
+    subplot(2,5,i)
+    errorbar(b{1}(:,i),stats{1}(i).se);
+    title(['Model 1, Neuron ' num2str(i)]);
+end
+
+% P-values
+for i = 1:length(b{1}(1,:))
+    disp(['P-values for model 1, Neuron ' num2str(i) ': ' num2str(stats{1}(1).p')]);
+end
+
+% AIC
+for i = 1:length(b{1}(1,:))
+    aic1(i) = dev{1}(i) + 2*length(b{1}(:,i));
+    disp(['AIC for Model 1, Neuron ' num2str(i) ': ' num2str(aic1(i))]);
+end
+    
+% KS-plot
+figure(101);
+clear b_lin dev_lin stats_lin lamdaEst KS
+for i = 1:length(spikes_binned(1,:))
+%     [b_lin(:,i),dev_lin(:,i),stats_lin(i)] = glmfit([xN yN xN.^2 yN.^2],spikes_binned(:,i),'poisson');
+%     lambdaEst = exp(b_lin(1,i) + b_lin(2,i)*xN + b_lin(3,i)*yN + b_lin(4,i)*xN.^2 + b_lin(5,i)*yN.^2); 
+
+    lambdaEst = exp(b{1}(1,i) + b{1}(2,i)*xN + b{1}(3,i)*yN + b{1}(4,i)*xN.^2 + b{1}(5,i)*yN.^2);
+    
+    timestep = 1;
+    lambdaInt = 0;
+    j=0;
+
+    for t=1:length(spikes_binned(:,i))
+        lambdaInt = lambdaInt + lambdaEst(t)*timestep;
+        if (spikes_binned(t,i))
+            j = j + 1;
+            KS(j,i) = 1-exp(-lambdaInt);
+            lambdaInt = 0;
+        end
+    end
+
+    KSSorted1 = sort( KS(:,i) );
+    N = length( KSSorted1);
+    figure(101);
+    subplot(2,5,i);
+    plot( ([1:N]-.5)/N, KSSorted1, 'b', 0:.01:1,0:.01:1, 'g',0:.01:1, [0:.01:1]+1.36/sqrt(N), 'r', 0:.01:1,[0:.01:1]-1.36/sqrt(N), 'r' );
+    axis( [0 1 0 1] );
+    xlabel('Uniform CDF');
+    ylabel('Empirical CDF of Rescaled ISIs');
+    title(['Model 1: Neuron ' num2str(i) ': KS Plot']);
+
+    ks_stat1(i) = max(abs(KSSorted1' - ((1:N)-.5)/N));
+    
+    disp(['Completed Neuron ' num2str(i) '.']);
+
+end
+
+
+disp(['KS Stats: ' num2str(ks_stat1)]);
+
+AIC{1} = aic1;
+ks_stat{1} = ks_stat1;
+KSSorted{1} = KSSorted1;
+
+%---------------- Model 2, Covariates: Sinusoids -------------------- 
+
+% Confidence intervals for each covariate
+figure(2);
+for i = 1:length(b{2}(1,:))
+    subplot(2,5,i)
+    errorbar(b{2}(:,i),stats{2}(i).se);
+    title(['Model 2, Neuron ' num2str(i)]);
+end
+
+% P-values
+for i = 1:length(b{2}(1,:))
+    disp(['P-values for model 2, Neuron ' num2str(i) ': ' num2str(stats{2}(1).p')]);
+end
+
+% AIC
+for i = 1:length(b{2}(1,:))
+    aic2(i) = dev{2}(i) + 2*length(b{2}(:,i));
+    disp(['AIC for Model 2, Neuron ' num2str(i) ': ' num2str(aic2(i))]);
+end
+
+% KS-plot
+figure(102);
+clear b_lin dev_lin stats_lin lamdaEst KS
+for i = 1:length(spikes_binned(1,:))
+
+    lambdaEst = exp(b{2}(1,i) + b{2}(2,i)*sin(2.5*pi*xN) + b{2}(3,i)*sin(2.5*pi*yN));
+    
+    timestep = 1;
+    lambdaInt = 0;
+    j=0;
+
+    for t=1:length(spikes_binned(:,i))
+        lambdaInt = lambdaInt + lambdaEst(t)*timestep;
+        if (spikes_binned(t,i))
+            j = j + 1;
+            KS(j,i) = 1-exp(-lambdaInt);
+            lambdaInt = 0;
+        end
+    end
+
+    KSSorted1 = sort( KS(:,i) );
+    N = length( KSSorted1);
+    figure(102);
+    subplot(2,5,i);
+    plot( ([1:N]-.5)/N, KSSorted1, 'b', 0:.01:1,0:.01:1, 'g',0:.01:1, [0:.01:1]+1.36/sqrt(N), 'r', 0:.01:1,[0:.01:1]-1.36/sqrt(N), 'r' );
+    axis( [0 1 0 1] );
+    xlabel('Uniform CDF');
+    ylabel('Empirical CDF of Rescaled ISIs');
+    title(['Model 2: Neuron ' num2str(i) ': KS Plot']);
+
+    ks_stat1(i) = max(abs(KSSorted1' - ((1:N)-.5)/N));
+    
+    disp(['Completed Neuron ' num2str(i) '.']);
+
+end
+
+
+disp(['KS Stats: ' num2str(ks_stat1)]);
+
+AIC{2} = aic2;
+ks_stat{2} = ks_stat1;
+KSSorted{2} = KSSorted1;
+
+%---------------- Model 3, Covariates: Refractory period history-dependence ---------------
+% Confidence intervals for each covariate
+figure(3);
+for i = 1:length(b{3}(1,:))
+    subplot(2,5,i)
+    errorbar(b{3}(:,i),stats{3}(i).se);
+    title(['Model 3, Neuron ' num2str(i)]);
+end
+
+% P-values
+for i = 1:length(b{3}(1,:))
+    disp(['P-values for model 3, Neuron ' num2str(i) ': ' num2str(stats{3}(1).p')]);
+end
+
+% AIC
+for i = 1:length(b{3}(1,:))
+    aic3(i) = dev{3}(i) + 2*length(b{3}(:,i));
+    disp(['AIC for Model 3, Neuron ' num2str(i) ': ' num2str(aic3(i))]);
+end
+
+% KS-plot
+figure(103);
+clear b_lin dev_lin stats_lin lamdaEst KS
+for i = 1:length(spikes_binned(1,:))
+    % Define history
+    lambh = zeros(length(spikes_binned(:,i))-5,length(spikes_binned(1:5,i)));
+    for k = 1:length(spikes_binned(1:5,i))
+        lambh(:,k) = spikes_binned((6-k):(end-k),i);
+    end
+    
+    % Lambda without history
+    lambdaEst = exp(b{3}(1,i));
+    % Add in history
+    for k = 2:length(b{3}(:,i))
+     lambdaEst = lambdaEst + b{3}(k,i)*lambh(:,k-1);
+    end
+    
+    timestep = 1;
+    lambdaInt = 0;
+    j=0;
+
+    for t=1:length(spikes_binned(:,i))-5
+        lambdaInt = lambdaInt + lambdaEst(t)*timestep;
+        if (spikes_binned(t,i))
+            j = j + 1;
+            KS(j,i) = 1-exp(-lambdaInt);
+            lambdaInt = 0;
+        end
+    end
+
+    KSSorted1 = sort( KS(:,i) );
+    N = length( KSSorted1);
+    figure(103);
+    subplot(2,5,i);
+    plot( ([1:N]-.5)/N, KSSorted1, 'b', 0:.01:1,0:.01:1, 'g',0:.01:1, [0:.01:1]+1.36/sqrt(N), 'r', 0:.01:1,[0:.01:1]-1.36/sqrt(N), 'r' );
+    axis( [0 1 0 1] );
+    xlabel('Uniform CDF');
+    ylabel('Empirical CDF of Rescaled ISIs');
+    title(['Model 2: Neuron ' num2str(i) ': KS Plot']);
+
+    ks_stat1(i) = max(abs(KSSorted1' - ((1:N)-.5)/N));
+    
+    disp(['Completed Neuron ' num2str(i) '.']);
+
+end
+
+
+disp(['KS Stats: ' num2str(ks_stat1)]);
+
+AIC{3} = aic3;
+ks_stat{3} = ks_stat1;
+KSSorted{3} = KSSorted1;
+
+%---------------- Model 4, Covariates: Short-term history-dependence ---------------
+% Confidence intervals for each covariate
+figure(4);
+for i = 1:length(b{4}(1,:))
+    subplot(2,5,i)
+    errorbar(b{4}(:,i),stats{4}(i).se);
+    title(['Model 4, Neuron ' num2str(i)]);
+end
+
+% P-values
+for i = 1:length(b{4}(1,:))
+    disp(['P-values for model 4, Neuron ' num2str(i) ': ' num2str(stats{4}(1).p')]);
+end
+
+% AIC
+for i = 1:length(b{4}(1,:))
+    aic4(i) = dev{4}(i) + 2*length(b{4}(:,i));
+    disp(['AIC for Model 4, Neuron ' num2str(i) ': ' num2str(aic4(i))]);
+end
+
+% KS-plot
+figure(104);
+clear b_lin dev_lin stats_lin lamdaEst KS
+for i = 1:length(spikes_binned(1,:))
+    % Define history
+    lambh = zeros(length(spikes_binned(:,i))-125,length(spikes_binned(75:125,i)));
+    for k = 1:length(spikes_binned(75:125,i))
+        lambh(:,k) = spikes_binned((126-k):(end-k),i);
+    end
+    
+    % Lambda without history
+    lambdaEst = exp(b{4}(1,i));
+    % Add in history
+    for k = 2:length(b{4}(:,i))
+     lambdaEst = lambdaEst + b{4}(k,i)*lambh(:,k-1);
+    end
+    
+    timestep = 1;
+    lambdaInt = 0;
+    j=0;
+
+    for t=1:length(spikes_binned(:,i))-125
+        lambdaInt = lambdaInt + lambdaEst(t)*timestep;
+        if (spikes_binned(t,i))
+            j = j + 1;
+            KS(j,i) = 1-exp(-lambdaInt);
+            lambdaInt = 0;
+        end
+    end
+
+    KSSorted1 = sort( KS(:,i) );
+    N = length( KSSorted1);
+    figure(104);
+    subplot(2,5,i);
+    plot( ([1:N]-.5)/N, KSSorted1, 'b', 0:.01:1,0:.01:1, 'g',0:.01:1, [0:.01:1]+1.36/sqrt(N), 'r', 0:.01:1,[0:.01:1]-1.36/sqrt(N), 'r' );
+    axis( [0 1 0 1] );
+    xlabel('Uniform CDF');
+    ylabel('Empirical CDF of Rescaled ISIs');
+    title(['Model 2: Neuron ' num2str(i) ': KS Plot']);
+
+    ks_stat1(i) = max(abs(KSSorted1' - ((1:N)-.5)/N));
+    
+    disp(['Completed Neuron ' num2str(i) '.']);
+
+end
+
+
+disp(['KS stats: ' num2str(ks_stat1)]);
+
+AIC{4} = aic4;
+ks_stat{4} = ks_stat1;
+KSSorted{4} = KSSorted1;
+
+
+%---------------- Model 5, Covariates: Long-term history-dependence ---------------
+% Confidence intervals for each covariate
+figure(5);
+for i = 1:length(b{5}(1,:))
+    subplot(2,5,i)
+    errorbar(b{5}(:,i),stats{5}(i).se);
+    title(['Model 5, Neuron ' num2str(i)]);
+end
+
+% P-values
+for i = 1:length(b{5}(1,:))
+    disp(['P-values for model 5, Neuron ' num2str(i) ': ' num2str(stats{5}(1).p')]);
+end
+
+% AIC
+for i = 1:length(b{5}(1,:))
+    aic5(i) = dev{5}(i) + 2*length(b{5}(:,i));
+    disp(['AIC for Model 5, Neuron ' num2str(i) ': ' num2str(aic5(i))]);
+end
+
+% KS-plot
+figure(105);
+clear b_lin dev_lin stats_lin lamdaEst KS
+for i = 1:length(spikes_binned(1,:))
+    % Define history
+    lambh = zeros(length(spikes_binned(:,i))-275,length(spikes_binned(200:275,i)));
+    for k = 1:length(spikes_binned(200:275,i))
+        lambh(:,k) = spikes_binned((276-k):(end-k),i);
+    end
+    
+    % Lambda without history
+    lambdaEst = exp(b{5}(1,i));
+    % Add in history
+    for k = 2:length(b{5}(:,i))
+     lambdaEst = lambdaEst + b{5}(k,i)*lambh(:,k-1);
+    end
+    
+    timestep = 1;
+    lambdaInt = 0;
+    j=0;
+
+    for t=1:length(spikes_binned(:,i))-275
+        lambdaInt = lambdaInt + lambdaEst(t)*timestep;
+        if (spikes_binned(t,i))
+            j = j + 1;
+            KS(j,i) = 1-exp(-lambdaInt);
+            lambdaInt = 0;
+        end
+    end
+
+    KSSorted1 = sort( KS(:,i) );
+    N = length( KSSorted1);
+    figure(105);
+    subplot(2,5,i);
+    plot( ([1:N]-.5)/N, KSSorted1, 'b', 0:.01:1,0:.01:1, 'g',0:.01:1, [0:.01:1]+1.36/sqrt(N), 'r', 0:.01:1,[0:.01:1]-1.36/sqrt(N), 'r' );
+    axis( [0 1 0 1] );
+    xlabel('Uniform CDF');
+    ylabel('Empirical CDF of Rescaled ISIs');
+    title(['Model 2: Neuron ' num2str(i) ': KS Plot']);
+
+    ks_stat1(i) = max(abs(KSSorted1' - ((1:N)-.5)/N));
+    
+    disp(['Completed Neuron ' num2str(i) '.']);
+
+end
+
+
+disp(['KS Stats: ' num2str(ks_stat1)]);
+
+AIC{5} = aic5;
+ks_stat{5} = ks_stat1;
+KSSorted{5} = KSSorted1;
+
+end
+
+
+
