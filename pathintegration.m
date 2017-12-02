9c05cb3903b1dae9ee2d165211ca81d5f5f36a8d
ssterrett <ssterre2@jhu.edu>
2017-12-01 19:56:07 -0500

    updated

--- pathintegration.m
+++ pathintegration.m
@@ -1,12 +1,25 @@
 function pathintegration(dataset)
 load(dataset);
 close all;
-steps = 50;
+steps = 25;
 for k = 1:10;
     
 ind = find(spikes_binned(:,k) == 1);
-figure(k); hold on;
+subplot(2,5,k); hold on;
+% axis square;
+store = zeros(length(ind),1);
 for i = 1:length(ind)
-    plot(xN(ind(i)-steps:ind(i))-xN(ind(i)),yN(ind(i)-steps:ind(i))-yN(ind(i)));
+    pos = [(xN(ind(i)-steps) - xN(ind(i))) (yN(ind(i) - steps) - yN(ind(i))) 0];
+    store(i) = angle((xN(ind(i)-steps) - xN(ind(i))) -1j*(yN(ind(i) - steps) - yN(ind(i))));
+%     plot([xN(ind(i)-steps)-xN(ind(i)) 0]./(sqrt((xN(ind(i)-steps)-xN(ind(i))).^2 + (yN(ind(i)-steps)-yN(ind(i))).^2)),[yN(ind(i)-steps)-yN(ind(i)) 0]./(sqrt((xN(ind(i)-steps)-xN(ind(i))).^2 + (yN(ind(i)-steps)-yN(ind(i))).^2)));
 end
+histogram(store);
+xticks([-pi 0 pi]);
+xticklabels({'-\pi' '0' '\pi'});
+ylabel('Spikes'); xlabel('Head Direction');
+title(['Neuron ' num2str(k)]);
+
+xNd = xN(50:end,k) - xN(1:(end-50),k);
+yNd = yN(50:end,k) - yN(1:(end-50),k);
+
 end
