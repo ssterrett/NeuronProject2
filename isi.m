0e04ec3ddc652b86b5fd0a9ac4b5c9bb0b05673c
wheatless <ireucroft@gmail.com>
2017-12-05 09:33:56 -0500

    Consolidated models. Added history and wall distance terms to model 1. Added speed term to all models. Made ISI bins display data better.

--- isi.m
+++ isi.m
@@ -15,17 +15,71 @@ for i = 1:neurons;
 spike_times{i} = find(spikes_binned(:,i) == 1);
 isi{i} = diff(spike_times{i});
 
-% Separate figures
-% figure(i); 
+if i==3
+    % Separate figures
+    % figure(i); 
 
-% One figure with 10 subplots
-subplot(2,5,i);
+    % One figure with 10 subplots
+    subplot(2,5,i);
 
-histogram(isi{i},10000);
-set(gca,'FontSize',14);
-xlim([0 500]);
-xlabel('ISI (ms)','FontSize',15);
-ylabel('Count','FontSize',15);
-title(['ISI Histogram for Neuron ' num2str(i)]);
+    histogram(isi{i},15000);
+    set(gca,'FontSize',14);
+    xlim([0 300]);
+    xlabel('ISI (ms)','FontSize',15);
+    ylabel('Count','FontSize',15);
+    title(['ISI Histogram for Neuron ' num2str(i)]);
+elseif i==4
+    % Separate figures
+    % figure(i); 
+
+    % One figure with 10 subplots
+    subplot(2,5,i);
+
+    histogram(isi{i},8000);
+    set(gca,'FontSize',14);
+    xlim([0 300]);
+    xlabel('ISI (ms)','FontSize',15);
+    ylabel('Count','FontSize',15);
+    title(['ISI Histogram for Neuron ' num2str(i)]);
+elseif i==6
+    % Separate figures
+    % figure(i); 
+
+    % One figure with 10 subplots
+    subplot(2,5,i);
+
+    histogram(isi{i}, 6000);
+    set(gca,'FontSize',14);
+    xlim([0 300]);
+    xlabel('ISI (ms)','FontSize',15);
+    ylabel('Count','FontSize',15);
+    title(['ISI Histogram for Neuron ' num2str(i)]);
+elseif i==7
+    % Separate figures
+    % figure(i); 
+
+    % One figure with 10 subplots
+    subplot(2,5,i);
+
+    histogram(isi{i},15000);
+    set(gca,'FontSize',14);
+    xlim([0 300]);
+    xlabel('ISI (ms)','FontSize',15);
+    ylabel('Count','FontSize',15);
+    title(['ISI Histogram for Neuron ' num2str(i)]);
+else
+    % Separate figures
+    % figure(i); 
+
+    % One figure with 10 subplots
+    subplot(2,5,i);
+
+    histogram(isi{i},10000);
+    set(gca,'FontSize',14);
+    xlim([0 300]);
+    xlabel('ISI (ms)','FontSize',15);
+    ylabel('Count','FontSize',15);
+    title(['ISI Histogram for Neuron ' num2str(i)]);
+end
 
 end
--- visualize.m
+++ visualize.m
@@ -10,7 +10,9 @@ end
 
 load(file);
 
+% Visualization of animal path and spike positions
 figure;
+
 for i = 1:length(spikes_binned(1,:))
     % Define x,y position where spikes occur
     x_at_spiketimes{i} = xN(find(spikes_binned(:,i)==1));
@@ -25,5 +27,26 @@ for i = 1:length(spikes_binned(1,:))
     title(['Spike positions, Neuron ' num2str(i)]);
     hold on;
 end
+
+% Histogram of spikes with respect to x,y position
+figure;
+for i = 1:length(spikes_binned(1,:))
+    % Define x,y position where spikes occur
+    x_at_spiketimes{i} = xN(find(spikes_binned(:,i)==1));
+    y_at_spiketimes{i} = yN(find(spikes_binned(:,i)==1));
+    
+    % Plot
+    subplot(2,5,i)
+    histogram2(x_at_spiketimes{i},y_at_spiketimes{i},'DisplayStyle','tile','ShowEmptyBins','on','FaceColor','flat');
+    caxis manual;
+    caxis([0 350]);
+    colorbar;
+    axis square
+    set(gca,'FontSize',14);
+    xlabel('x position (m)','FontSize',18); ylabel('y position(m)', 'FontSize',18);
+    title(['Spiking Histogram, Neuron ' num2str(i)]);
+    
+end
+
 
 end
